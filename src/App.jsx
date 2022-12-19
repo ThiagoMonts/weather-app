7ee18f9bdc6ca765038b86b4406d7aba99a0240f
ThiagoMonts <thiagohonorato@hotmail.com>
2022-12-19 11:27:19 -0300

    Estilizando o form

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
 function App() {
-  return <h1>Olá, Mundo!</h1>;
+  return (
+    <div className="flex w-full h-screen items-center justify-center">
+      <form>
+        <input
+          type="text"
+          placeholder="Cidade"
+          className="p-3 rounded-lg outline-none"
+        />
+        <button
+          type="submit"
+          className="bg-blue-600 p-3 rounded-lg ml-3 text-white font-bold"
+        >
+          Pesquisar
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
